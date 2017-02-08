150441f0a110a47b329bd675114952ab1c714abf
Ashwini <ashie.oh@gmail.com>
2017-02-08 17:57:52 +0000

    generalised test cases describtion

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,24 +1,24 @@
 require './lib/fizzbuzz'
 describe "fizzbuzz" do
-  it "returns 'fizz' when passed 3" do
+  it 'returns the message "fizz", when passed a number that is a multiple of 3' do
     expect(fizzbuzz(3)).to eq "fizz"
     expect(fizzbuzz(6)).to eq "fizz"
   end
 end
 describe "fizzbuzz" do
-  it "returns 'buzz' when passed 5" do
+  it 'returns the message "buzz", when passed a number that is a multiple of 5' do
     expect(fizzbuzz(5)).to eq "buzz"
     expect(fizzbuzz(10)).to eq "buzz"
   end
 end
 describe "fizzbuzz" do
-  it "returns 'fizzbuzz' when passed 15" do
+  it "returns the message 'fizzbuzz', when passed a number that is a multiple of 3 & 5" do
     expect(fizzbuzz(15)).to eq "fizzbuzz"
     expect(fizzbuzz(30)).to eq "fizzbuzz"
   end
 end
 describe "fizzbuzz" do
-  it "returns 1 when passed 1" do
+  it "returns the number when any other number is passed" do
     expect(fizzbuzz(1)).to eq 1
     expect(fizzbuzz(7)).to eq 7
   end
